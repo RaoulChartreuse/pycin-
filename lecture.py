803ddea06531a8dce2bc3969920598a57a8ce4b8
RaoulChartreuse <laurent.morin.morin@gmail.com>
2018-09-21 11:33:01 +0200

    Add log scale

--- lecture.py
+++ lecture.py
@@ -11,18 +11,15 @@ filename= args.input
 
 #r=np.load(filename)
 #Ecriture de la version c++
-r=np.loadtxt(filename)
+r=np.fromfile(filename,sep=" ")
     
 x=np.arange(r.size)
 print r,x
 
 
 fig, ax = plt.subplots(figsize=(8, 4))
-ax.hist(-r[~np.isnan(r)],200,histtype='step',
-        cumulative=True)
-#ax.plot(x,r)
+ax.hist(r[~np.isnan(r)],200,histtype='step')
 
-
-
+ax.set_yscale("log", nonposy='clip')
 
 plt.show()
